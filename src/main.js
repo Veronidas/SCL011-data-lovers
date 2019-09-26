a942ae2ed2853762dc1bf0668842938eddaf5656
veronidas <veronabrkich@gmail.com>
2019-09-25 22:14:44 -0300

    Actualizacion del readme, mostrar data, definicion de producto

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 /* Manejo del DOM */
+const allPokemon = window.POKEMON.pokemon;
+let data = Object.values(allPokemon);
+
+document.getElementById("root").innerHTML = `<div class="fila">
+<div class="col">Imagenes</div>
+<div class="col">Nombre</div>
+<div class="col">#</div>
+<divclass="col">Tipo</div>
+</div>`
+data.forEach(element => {
+    document.getElementById("root").innerHTML += `<div class="fila">
+    <div class="col"><img src="${element.img}"</div>
+    <div class="col">${element.num}</div>
+    <div class="col">${element.type}</div>
+    </div>
+    `
+});
+
+
